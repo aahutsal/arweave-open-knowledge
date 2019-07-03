5b875770dbfef09da89f14cac83ac115ffb3b82d
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2019-07-03 10:12:37 +0300

    jest set up
    merged with master
    
    merged with master

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,24 @@
 module.exports = {
+  "globals": {
+		'ts-jest': {
+			tsConfig: './tsconfig.json',
+		},
+		stringifyContentPathRegex: true,
+  },
   "roots": [
     "<rootDir>/src"
   ],
   "transform": {
-    "^.+\\.tsx?$": "ts-jest"
+    ".(ts|tsx)": "ts-jest"
+  },
+  "testRegex": "\\.(test|spec)\\.(ts|tsx|js)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js"
   },
 }
